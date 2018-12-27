11f714e564b39b83ca5c2b91d83bb71ba915a5f3
Your Name <you@example.com>
2018-12-27 15:40:57 +0800

    借阅书籍

--- app/src/main/java/com/example/stu/library/JYshujiActivity.java
+++ app/src/main/java/com/example/stu/library/JYshujiActivity.java
@@ -3,11 +3,11 @@ package com.example.stu.library;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class JYshujieActivity extends AppCompatActivity {
+public class JYshujiActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_jyshujie);
+        setContentView(R.layout.activity_jyshuji);
     }
 }
