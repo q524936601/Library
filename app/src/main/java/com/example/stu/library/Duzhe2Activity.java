042477c0ab2d4e85dfed2fd53bdf85eb2a8fd771
Your Name <you@example.com>
2018-12-27 14:37:41 +0800

    读者登录

---
+++ app/src/main/java/com/example/stu/library/Duzhe2Activity.java
@@ -0,0 +1,13 @@
+package com.example.stu.library;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Duzhe2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duzhe2);
+    }
+}
