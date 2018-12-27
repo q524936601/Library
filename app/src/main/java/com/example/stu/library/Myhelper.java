701e325da0335af6124d2a9f2b9dd2edba02d65f
Your Name <you@example.com>
2018-12-27 17:16:26 +0800

    数据库

---
+++ app/src/main/java/com/example/stu/library/Myhelper.java
@@ -0,0 +1,21 @@
+package com.example.stu.library;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+
+public class Myhelper extends SQLiteOpenHelper {
+    public Myhelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql="create table information(_id integer primary key autoincrement,name varchar(20),zuozhe varchar(20),chubanshe varchar(20),number,integer)";
+        db.execSQL(sql);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+    }
+
