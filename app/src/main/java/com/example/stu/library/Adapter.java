3388556fd524ecd37959789927b3541cafe92bc0
Your Name <you@example.com>
2018-12-28 16:13:40 +0800

    适配器与功能

--- app/src/main/java/com/example/stu/library/Adapter.java
+++ app/src/main/java/com/example/stu/library/Adapter.java
@@ -1,5 +1,82 @@
 package com.example.stu.library;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.*;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Adapter {
+    SQLiteDatabase db=null;
+    Context context=null;
+
+    public Adapter(Context context){this.context=context;}
+
+    public void openDB(){
+        Myhelper myhelper=new Myhelper(context,"datavase.db",null,1);
+        db=myhelper.getWritableDatabase();
+    }
+    public void closeDB(){
+        if (db.isOpen()){
+            db.close();
+        }
+        db=null;
+    }
+    public boolean insert(Infor infor){
+        boolean result =false;
+        openDB();
+        ContentValues values=new ContentValues();
+        values.put("number",infor.getNumber());
+        values.put("name",infor.getName());
+        values.put("zhuozhe",infor.getZuozhe());
+        values.put("chubanshe",infor.getChubanshe());
+
+        Long rowid=db.insert("information",null,values);
+        if (rowid!=-1){
+            result=true;
+            Toast.makeText(context,"添加成功",Toast.LENGTH_SHORT).show();
+            Log.i("数据库操作","添加成功");
+        }else {
+            Toast.makeText(context,"添加失败",Toast.LENGTH_SHORT).show();
+            Log.i("数据库操作","添加失败");
+        }
+        closeDB();
+        return result;
+    }
+
+    public List<Infor> queryAll(){
+        List<Infor>list=new ArrayList<Infor>();
+        openDB();
+        Cursor cursor=db.query("information",null,null,null,null,
+                null,null);
+        if (cursor.moveToFirst()){
+            do {
+                long number=cursor.getLong(cursor.getColumnIndex("number"));
+                String name=cursor.getString(cursor.getColumnIndex("name"));
+                String zuozhe=cursor.getString(cursor.getColumnIndex("zuozhe"));
+                String chubanshe=cursor.getString(cursor.getColumnIndex("chubanshe"));
+                Infor infor=new Infor();
+                infor.setNumber(number);
+                infor.setName(name);
+                infor.setZuozhe(zuozhe);
+                infor.setChubanshe(chubanshe);
+            }while (cursor.moveToNext());
+        }
+        closeDB();
+        return list;
+    }
+
+    public int delete(String name){
+        int result=-1;
+        openDB();
+        result=db.delete("information","name=?",new String[]{name});
+        closeDB();
+        return result;
+    }
+
 
 }
--- app/src/main/java/com/example/stu/library/MainActivity.java
+++ app/src/main/java/com/example/stu/library/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
     Button btntianjia,btnchaxun,btnexit,btnshanchu;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         btnchaxun=findViewById(R.id.btn_main_chaxun);
         btnchaxun.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
         btnshanchu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,ChaxunActivity.class);
+                Intent intent=new Intent();
+                intent.setClass(MainActivity.this,ChaxunActivity.class);
                 startActivity(intent);
             }
         });
